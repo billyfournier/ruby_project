49a3c77038c5ae1d979f5cc7c56051fc3c8cdc35
billyfournier <billyfournier2000@yahoo.com>
2016-10-25 12:18:07 -0500

    update

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by_email(params[:Email])
+
+    if user && user.authenticate(params[:Password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)          # Go here if login is successful
+    else
+      flash[:errors] = ["Invalid combination"]
+      redirect_to sessions_new_path
+    end
+  end
+  def destroy
+    session[:user_id] = nil
+    redirect_to sessions_new_path
+  end
+end
